f064dbba2bab8c273273e9050cf69b597b0bd0a7
Ruilong25 <t105360025@ntut.org.tw>
2018-11-15 15:29:30 +0800

    Lab8

---
+++ P16/src/P16/P16.java
@@ -0,0 +1,12 @@
+package P16;
+
+public class P16 {
+
+	public static void main(String[] args) {
+		System.out.println("A");
+		  System.out.println("開始使用java!");
+	          System.out.println(123);
+
+	}
+
+}
---
+++ P26/src/P26/P26.java
@@ -0,0 +1,17 @@
+package P26;
+
+public class P26 {
+
+	public static void main(String[] args) {
+		int num = 3;
+
+        System.out.println("變數num的值是"+num);
+
+        num = 5;
+
+        System.out.println("變更變數num的值");
+        System.out.println("變數num更新後的值是"+num);
+
+	}
+
+}
